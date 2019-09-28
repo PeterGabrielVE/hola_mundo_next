f3c3cb8205cfc6acafc5da44ffdf5fa504705d64
Pedro Gabriel <gaboleal123@gmail.com>
2019-09-28 16:54:53 -0430

    Pagina 1 y renderizado

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
+
+function Figura1(){
+	return(
+		<img src="./static/logo-javascript.png" alt="" width="200px"/>
+		)
+}
+
+
+
+function Seccion1(){
+	return(
+		<section>
+			<h2>Esta es la seccion 1</h2>
+			<Figura1/>
+		</section>
+		)
+}
+
+
 function Index(){
 	return(
 		<div>
 			<h1>Bienvenidos a mi App con NextJS</h1>
 			<p>Esta es mi primera aplicación con NextJS</p>
+			<Seccion1/>
 			</div>
 			)
 }
