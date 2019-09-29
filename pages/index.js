f4db466feda8d6e4c949a3f3c182fd0f651f11b7
Pedro Gabriel <gaboleal123@gmail.com>
2019-09-29 16:10:04 -0430

    Composicion de funciones y componente estatico

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,17 @@
+const Contenedor = (props)=>(
+	<div style={{
+		background:"#392FFF",
+		textAlign:"center",
+		boder:"1px solid",
+		width:"800px",
+		margin:"0 auto",
+		color:"#FFFFFF",
+		padding:"20px",
+	}}>
+		{props.children}
+	</div>
 
+	)
 function Figura1(){
 	return(
 		<img src="./static/logo-javascript.png" alt="" width="200px"/>
@@ -19,11 +32,11 @@ function Seccion1(){
 
 function Index(){
 	return(
-		<div>
+		<Contenedor>
 			<h1>Bienvenidos a mi App con NextJS</h1>
 			<p>Esta es mi primera aplicación con NextJS</p>
 			<Seccion1/>
-			</div>
+			</Contenedor>
 			)
 }
 
